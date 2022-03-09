5f178bbee04440dc895ef2f312e41ffa2349d914
baky2708 <flavioisaque123@gmail.com>
2022-03-09 13:18:56 -0300

    restart: start new layout and project

--- src/components/Home/PaintMenu/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Menu = styled.div`
-  background-color: blue;
-  width: 25px;
-  height: 400px;
-`;
-
-export default Menu;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,14 +6,10 @@ import {
 } from 'react-router-dom';
 
 import Home from '../pages/Home';
-import ResetCss from '../GlobalStyles/ResetCss';
-import GlobalStyles from '../GlobalStyles';
 
 function Routes() {
   return (
     <BrowserRouter>
-      <ResetCss />
-      <GlobalStyles />
       <Switch>
         <Route path="/" exact component={Home} />
       </Switch>
